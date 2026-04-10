e3b5d1659670cc37507fa18da26430a145f806f1
GuilhermeLimaDev <guilhermeguiguigugu2509@gmail.com>
2026-04-09 23:53:40 -0300

    Iniciando o projeto

---
+++ src/test/java/io/github/guilhermelimadev/resume/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermelimadev.resume.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
